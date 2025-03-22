25da1431859e0dcd0478933ccacb1fc9fcc1974e
Mürsel SEN <murselsen_35@hotmail.com>
2025-03-22 14:18:41 +0300

    Enhance timer functionality: disable start button initially and include script for timer logic

--- src/js/01-timer.js
+++ src/js/01-timer.js
@@ -1,2 +1,41 @@
 // 01-timer.js
 
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+let userSelectedDate;
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+flatpickr('#datetime-picker', {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  mode: 'range',
+  onClose(selectedDates) {
+    console.log(selectedDates);
+    let defaultDate = selectedDates[0];
+    userSelectedDate = selectedDates[1];
+    console.log(Date.parse(defaultDate));
+    console.log(Date.parse(userSelectedDate));
+  },
+});
+let result = convertMs(userSelectedDate);
